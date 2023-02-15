26ce6da369518eaacc56cce266972e4afe8aba25
9ns2 <9ns2@fresnou.org>
2023-02-15 09:40:28 -0800

    initial commit

---
+++ conditionals.js
@@ -0,0 +1,26 @@
+// Conditionals are if-statements
+
+let x=5
+if (x==5){
+    console.log("its 5!");
+}
+console.log("end of program");
+
+let z = 5
+if (z==5) {
+    console.log("option1");
+}else {
+    console.log("option2");
+}
+console.log("end of program");
+
+let num2 = 0
+if (num2==10){
+    console.log("equals 10");
+}else{
+    console.log("greater than 5")
+}
+       console.log("end of program");
+}
+
+
---
+++ input.js
@@ -0,0 +1,9 @@
+//1
+
+const readLine = require("readLine-sync");
+let answer = readLine.question("Do you want to hear a joke?" + "Yes or No");
+if (answer == "yes") {
+    console.log(`What is the best thing Switzerland.\n I dont know,but the flag is a big plus`) }
+else if (answer == "no") {
+    console.log("invalid");
+}
