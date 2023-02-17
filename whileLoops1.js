8b0dd69758a2267591d80c3dd7a0257d4cb0d28a
9ns2 <9ns2@fresnou.org>
2023-02-17 10:27:22 -0800

    initial commit

---
+++ whileLoops1.js
@@ -0,0 +1,27 @@
+const readLine = require('readline-sync');
+
+//1
+let x = 0;
+while(x<25){
+    console.log(x);
+    x++;
+}
+//2
+let y = 0;
+while(y<=50){
+    console.log(y);
+    y++;
+}
+//3
+let z = -10;
+while(z<=10){
+    console.log(z);
+    z++;
+}
+//4
+let f = 0;
+while(f>-51){
+    console.log(f);
+    f--;
+}
+//5
